794ca9ef05c1a76741a13a5a10df7b1158f2c6b0
SeanglyDev <Seangly.Dev@gmail.com>
2026-03-20 23:15:27 +0700

    Create_project

---
+++ Cinema-backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from Express + TypeScript!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}🚀✅`);
+});
